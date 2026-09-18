b782793ca9d6114d25ab5d47e05f0fcbf3224c69
NerdCow <84755104+TheNerdCow@users.noreply.github.com>
2026-09-18 15:57:21 +0330

    آپدیت 3.2.10

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -27,7 +27,7 @@ import {createWorkerProxyConfig, handleProxyRequest} from './proxy.js'
 import {decodeAddonConfig, mergeEnv} from './config.js'
 
 const ADDON_PREFIX = 'ip'
-const ADDON_VERSION = '3.2.9'
+const ADDON_VERSION = '3.2.10'
 
 const CATALOGS = [
     {key: 'f2media', name: 'F2Media', catalogType: 'movies'},
